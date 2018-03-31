e059d3d3486192a270a7c168d899af597803078e
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-31 16:41:51 -0400

    changed original click function to an on-click, and changed the targets of everything to the ul, with the values passed through as: listener, item to manipulate, and callback - so that the functionality will work on dynamically created elements too

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 //Strike Through Specific Items When Clicked:
-$("li").click(() => {
+$("ul").on("click", "li", () => {
   $(event.target).toggleClass("checked")
 })
 
 //Delete ToDo Items:
-$("span").on("click", (event) => {
+$("ul").on("click", "span", (event) => {
   $(event.target).parent().fadeOut(500, () => {
     $(this).remove( )
   })
@@ -13,9 +13,9 @@ $("span").on("click", (event) => {
 
 //Submit on 'enter'
 $("input[type='text']").on("keypress", (event) => {
-  if (event.charCode == 13) {
+  if (event.which == 13) {
     let newItem = $(event.target).val()
-    $("<li/>").text(newItem).appendTo("#list")
+    $("<li><span>X</span> " + newItem + "</li>").appendTo("#list")
     $(event.target).val("")
   }
 })
