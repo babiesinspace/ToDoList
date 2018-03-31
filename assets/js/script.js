63483a30d659c8e248fb6658315ad300261fed93
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-31 15:56:31 -0400

    switch previous to class and toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,7 @@
 //Strike Through Specific Items When Clicked:
 
 $("li").click(() => {
-  let item = $(event.target)
-  if ($(item).css("color") === "rgb(128, 128, 128)") {
-    $(item).css({
-    color: "black",
-    textDecoration: "none"})
-  } else {
-    $(event.target).css({
-      color: "gray",
-      textDecoration: "line-through"})
-  }
+  $(event.target).toggleClass("checked")
 })
 
 //Delete ToDo Items:
