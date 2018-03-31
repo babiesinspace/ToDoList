8dc4451a8c132d4419c787486387bc322201ea53
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-31 16:32:58 -0400

    add keypress event to add items to list by typing enter key, then clear input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //Strike Through Specific Items When Clicked:
-
 $("li").click(() => {
   $(event.target).toggleClass("checked")
 })
@@ -7,7 +6,16 @@ $("li").click(() => {
 //Delete ToDo Items:
 $("span").on("click", (event) => {
   $(event.target).parent().fadeOut(500, () => {
-    $(this).remove()
+    $(this).remove( )
   })
   event.stopPropagation()
+})
+
+//Submit on 'enter'
+$("input[type='text']").on("keypress", (event) => {
+  if (event.charCode == 13) {
+    let newItem = $(event.target).val()
+    $("<li/>").text(newItem).appendTo("#list")
+    $(event.target).val("")
+  }
 })
