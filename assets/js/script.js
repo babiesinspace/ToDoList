5d3e72a2a18d6157c8cb90680e93a18941f9bc6a
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-31 15:50:30 -0400

    add click listener to li items - user can check off items as 'finished' and click again to undo, before deleting them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-$("<span>")
+//Strike Through Specific Items When Clicked:
+
+$("li").click(() => {
+  let item = $(event.target)
+  if ($(item).css("color") === "rgb(128, 128, 128)") {
+    $(item).css({
+    color: "black",
+    textDecoration: "none"})
+  } else {
+    $(event.target).css({
+      color: "gray",
+      textDecoration: "line-through"})
+  }
+})
+
+//Delete ToDo Items:
+$("span").on("click", (event) => {
+  $(event.target).parent().remove()
+})
